7b84712d6c261b7a25f42661201c5ddae1835051
yasa579 <bandengprestolaut@gmail.com>
2025-09-26 12:51:13 +0700

    Update 1.1.2
    sw.js untuk kebutuhan update PWA+Download

---
+++ sw.js
@@ -0,0 +1,56 @@
+const CACHE_NAME = 'we9-music-player-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/BGS.mp4',
+  '/Matematika.mp3',
+  '/Mimpi.mp3',
+  '/Kunang-Kunang.mp3',
+  '/Memori-Suara.mp3',
+  '/Revolusi.mp3',
+  '/Download.svg',
+  '/icon-192x192.png',
+  '/icon-512x512.png'
+];
+
+// Install event
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch event
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// Activate event
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
